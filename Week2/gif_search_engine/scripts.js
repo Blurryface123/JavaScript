cc9aa8ecfdfe834446bd79d5939c17f9b85e77ef
alopecol <andrelopezcollantes21@gmail.com>
2019-10-18 14:49:51 +0200

    Conneted to giphy api

--- Week2/gif_search_engine/scripts.js
+++ Week2/gif_search_engine/scripts.js
@@ -1,41 +1,52 @@
 //GRAB THE INPUT VALUE
 
 let button = document.querySelector(".js-go")
-button.addEventListener("click",function(){
-    let input = document.querySelector("input").value;
-    console.log(input);
-    pushToDOM(input)
+button.addEventListener("click", function () {
+  let input = document.querySelector("input").value;
+  console.log(input);
+  pushToDOM(input)
 });
 
-document.querySelector(".js-userinput").addEventListener('keyup',function(e){
+document.querySelector(".js-userinput").addEventListener('keyup', function (e) {
 
-    var input = document.querySelector("input").value;
- 
- 
-   // if the key ENTER is pressed...
-   if(e.which === 13) {
-     pushToDOM(input);
-   }
- 
- });
+  var input = document.querySelector("input").value;
+
+
+  // if the key ENTER is pressed...
+  if (e.which === 13) {
+    pushToDOM(input);
+  }
+
+});
 
 //API
 
-var url = "api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=BpKaq8bodOahwIS1A9sTHPlesY4EYCkb";
+var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=BpKaq8bodOahwIS1A9sTHPlesY4EYCkb&limit=5";
 
 // AJAX Request
 var GiphyAJAXCall = new XMLHttpRequest();
-GiphyAJAXCall.open( 'GET', url );
+GiphyAJAXCall.open('GET', url);
 GiphyAJAXCall.send();
 
-GiphyAJAXCall.addEventListener('load',function(e){
+GiphyAJAXCall.addEventListener('load', function (e) {
 
-    var data = e.target.response;
-  console.log(data);
+  var data = e.target.response;
+  pushToDOM(data);
+  //console.log(data);
 
 });
+
 //SHOWS THE GIF
-function pushToDOM(input){
+function pushToDOM(input) {
+
+  var response = JSON.parse(input)
+  var imageURL = response.data;
+  console.log(imageURL)
+
+  imageURL.forEach(function (image) {
+    var src = image.images.fixed_height.url;
     var container = document.querySelector(".js-container");
-    container.innerHTML = input;
+    container.innerHTML += "<img src=\"" + src + "\">";
+
+  });
 }
